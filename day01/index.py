429d69e3168b0f928ea73599457ad6959141ee37
savasgormus <savasgormus1@gmail.com>
2022-02-24 11:06:14 +0300

    update

--- day01/index.py
+++ day01/index.py
@@ -1,3 +1,2 @@
-obeb = lambda x,y : [i for i in range(min(x,y),0-1)
-                        if x%i == y%i == 0][0]
-print(obeb(12,16))
+gcd = lambda x,y : [i for i in range(min(x,y),0,-1) if x%i == y%i == 0][0]
+print(gcd(120,90))
